04abf7e1c6c95166e413a41aea3e27ac547da55d
Asad Raza <asad.raza@sap.com>
2023-08-20 04:54:40 +0200

    Added JSDoc for Genre Service and Movie Controller

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -5,26 +5,53 @@ import { Genre } from './../schemas/genre.schema';
 import { Movie } from './../schemas/movie.schema';
 import { CreateGenreDto } from './dto/create-genre.dto';
 
+/**
+ * Service managing operations related to genres.
+ */
 @Injectable()
 export class GenreService {
+  /**
+   * Creates an instance of the GenreService.
+   * @param {Model<Genre>} genreModel - The Mongoose model for genres.
+   * @param {Model<Movie>} movieModel - The Mongoose model for movies.
+   */
   constructor(
     @InjectModel(Genre.name) private genreModel: Model<Genre>,
     @InjectModel(Movie.name) private movieModel: Model<Movie>,
   ) {}
 
+  /**
+   * Creates a new genre.
+   * @param {CreateGenreDto} createGenreDto - Data Transfer Object for genre creation.
+   * @returns {Promise<Genre>} A promise that resolves to the created genre.
+   */
   async create(createGenreDto: CreateGenreDto): Promise<Genre> {
     const createdGenre = new this.genreModel(createGenreDto);
     return await createdGenre.save();
   }
 
+  /**
+   * Retrieves all genres.
+   * @returns {Promise<Genre[]>} A promise that resolves to an array of genres.
+   */
   async findAll(): Promise<Genre[]> {
     return await this.genreModel.find().exec();
   }
 
+  /**
+   * Retrieves a specific genre by its ID.
+   * @param {string} id - The ID of the genre to retrieve.
+   * @returns {Promise<Genre>} A promise that resolves to the retrieved genre.
+   */
   async findOne(id: string): Promise<Genre> {
     return await this.genreModel.findById(id).exec();
   }
 
+  /**
+   * Deletes a specific genre by its ID and removes the genre from associated movies.
+   * @param {string} id - The ID of the genre to delete.
+   * @returns {Promise<void>} A promise that resolves when the deletion operation is complete.
+   */
   async remove(id: string): Promise<void> {
     const genre = await this.genreModel.findByIdAndDelete(id).exec();
 
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -12,15 +12,33 @@ import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 
+/**
+ * Controller handling routes related to movies.
+ */
 @Controller('movies')
 export class MovieController {
+  /**
+   * Creates an instance of the MovieController.
+   * @param {MovieService} movieService - Service managing movie operations.
+   */
   constructor(private readonly movieService: MovieService) {}
 
+  /**
+   * Endpoint to create a new movie.
+   * @param {CreateMovieDto} createMovieDto - Data Transfer Object for movie creation.
+   * @returns The created movie.
+   */
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.movieService.create(createMovieDto);
   }
 
+  /**
+   * Endpoint to retrieve all movies with pagination.
+   * @param {number} page - The page number.
+   * @param {number} limit - The number of movies per page.
+   * @returns An array of movies for the specified page and limit.
+   */
   @Get()
   findAll(@Query('page') page: number, @Query('limit') limit: number) {
     page = page > 0 ? page : 1;
@@ -28,21 +46,42 @@ export class MovieController {
     return this.movieService.findAll(page, limit);
   }
 
+  /**
+   * Endpoint to retrieve a specific movie by its ID.
+   * @param {string} id - The ID of the movie to retrieve.
+   * @returns The movie with the specified ID.
+   */
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.movieService.findOne(id);
   }
 
+  /**
+   * Endpoint to update a specific movie by its ID.
+   * @param {string} id - The ID of the movie to update.
+   * @param {UpdateMovieDto} updateMovieDto - Data Transfer Object for movie update.
+   * @returns The updated movie.
+   */
   @Put(':id')
   update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
     return this.movieService.update(id, updateMovieDto);
   }
 
+  /**
+   * Endpoint to delete a specific movie by its ID.
+   * @param {string} id - The ID of the movie to delete.
+   * @returns The result of the deletion operation.
+   */
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.movieService.remove(id);
   }
 
+  /**
+   * Endpoint to search movies by title or genre.
+   * @param {string} query - The search query.
+   * @returns An array of movies matching the search query.
+   */
   @Get('search/:query')
   search(@Param('query') query: string) {
     return this.movieService.search(query);
